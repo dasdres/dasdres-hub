d953dcc6f7098681a35bea7951b02e1a1e189360
dasdres <137342122+dasdres@users.noreply.github.com>
2023-06-24 22:40:22 +0300

    Update AWScript.lua

--- AWScript.lua
+++ AWScript.lua
@@ -1,35 +1,5 @@
 if game.PlaceId == 8260276694 then
 
-   function hitboxexpand()
-      local plyr = game.Players.LocalPlayer     
-      local char = plyr.Character
-      local Hitbox = char.Hitbox
-      
-      Hitbox.Size = Vector3.new(20, 20 ,20)
-      Hitbox.Transparency = 0.5
-      Hitbox.Material = "Water"
-   
-      end
-   
-   function hitboxshrink()
-      local plyr = game.Players.LocalPlayer     
-      local char = plyr.Character
-      local Hitbox = char.Hitbox
-      
-      Hitbox.Size = Vector3.new(1.5, 1.5 ,2)
-      Hitbox.Transparency = 1
-      Hitbox.Material = "Plastic"
-   
-      end
-      
-   
-          
-   
-
-   
-   
-   
-   
    function antivoid()
       local AVPart = Instance.new("Part", workspace)
       AVPart.Name = "AntiVoidPart1"
@@ -59,10 +29,7 @@ if game.PlaceId == 8260276694 then
       LocalPlayer.Character.Head["Name Tag"]:Destroy()
       end
    
-   
-   
-   
-   
+
    
    local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
    
@@ -110,22 +77,44 @@ if game.PlaceId == 8260276694 then
    },
    })
    
-   local HitboxExpandButton = StuffTab:CreateButton({
-      Name = "Expand Hitbox",
-      Callback = function()
-      hitboxexpand()
-      
+   local HitboxToggle = StuffTab:CreateToggle({
+      Name = "Expand Hitboxes",
+      CurrentValue = false,
+      Flag = "HBT", 
+      Callback = function(Value)
+         getgenv().HitboxExpand = Value
+
+
+         while HitboxExpand == true do
+            task.wait(0.1)
+        for i, Player in pairs(game.Players:GetPlayers()) do
+            local allplyrschar = Player.Character or Player.CharacterAdded or game.workspace[Player.Name]
+            local HumanoidRootPart = allplyrschar:GetChildren("HumanoidRootPart")
+   
+                if allplyrschar and Player ~= game.Players.LocalPlayer and HumanoidRootPart then
+                   allplyrschar:WaitForChild("HumanoidRootPart").Size = Vector3.new(20, 20, 20)
+                   allplyrschar:WaitForChild("HumanoidRootPart").Transparency = 0.5
+                end 
+   
+               end
+                task.wait()
+            end
+            
+        
+   
+        if HitboxExpand == false then
+           for i, Player in pairs(game.Players:GetPlayers()) do
+               local allplyrschar = Player.Character or Player.CharacterAdded or game.workspace[Player.Name]
+                   if allplyrschar and Player ~= game.Players.LocalPlayer and game.workspace[Player.Name].HumanoidRootPart then
+                      allplyrschar.HumanoidRootPart.Size = Vector3.new(2, 2, 1)
+                      allplyrschar.HumanoidRootPart.Transparency = 1
+                   end 
+                   
+               end
+           end
       end,
    })
-   
-   local NormalButton = StuffTab:CreateButton({
-      Name = "Normal HitBox",
-      Callback = function()
-      hitboxshrink()
-      
-      end,
-   })
-   
+
    local DNTButton = StuffTab:CreateButton({
       Name = "Destroy Nametag",
       Callback = function()
@@ -173,51 +162,143 @@ if game.PlaceId == 8260276694 then
    
    local BossTab = Window:CreateTab("Bosses", 4483362458) -- Title, Image
    
-   local TreeLordButton = BossTab:CreateButton({
-      Name = "Tree Lord HitBox",
-      Callback = function()
-      game.workspace["Tree Lord"].HumanoidRootPart.Size = Vector3.new(50, 50, 50)
-      game.workspace["Tree Lord"].HumanoidRootPart.Transparency = 0.5
-     
+   local TreeLordToggle = BossTab:CreateToggle({
+      Name = "Tree Lord Hitbox",
+      CurrentValue = false,
+      Flag = "TLHT", 
+      Callback = function(Value)
+          getgenv().TreeLordHitbox = Value
+          while TreeLordHitbox == true do
+          for i, v in pairs(game.workspace:GetChildren()) do
+          if v.Name == "Tree Lord" and v:FindFirstChild("HumanoidRootPart") then
+           game.workspace["Tree Lord"].HumanoidRootPart.Size = Vector3.new(50, 50, 50)
+           game.workspace["Tree Lord"].HumanoidRootPart.Transparency = 0.5
+          end
+      end
+      wait()
+  end
+  if TreeLordHitbox == false then
+   for i, v in pairs(game.workspace:GetChildren()) do 
+      if v.Name == "Tree Lord" and v:FindFirstChild("HumanoidRootPart") then
+         game.workspace["Tree Lord"].HumanoidRootPart.Size = Vector3.new(10.8166, 10.8166, 5.40831)
+         game.workspace["Tree Lord"].HumanoidRootPart.Transparency = 1
+        end
+    end
+   end
+
+
       end,
    })
-   
-   local MushroomKingButton = BossTab:CreateButton({
+   local MushroomKingToggle = BossTab:CreateToggle({
       Name = "Mushroom King Hitbox",
-      Callback = function()
-       game.workspace["Mushroom King"].HumanoidRootPart.Size = Vector3.new(50, 50, 50)
-      game.workspace["Mushroom King"].HumanoidRootPart.Transparency = 0.5
-      
+      CurrentValue = false,
+      Flag = "MKHT", 
+      Callback = function(Value)
+          getgenv().MushKingHitbox = Value
+          while MushroomKingHitbox == true do
+          for i, v in pairs(game.workspace:GetChildren()) do
+          if v.Name == "Mushroom King" and v.HumanoidRootPart then
+           game.workspace["Mushroom King"].HumanoidRootPart.Size = Vector3.new(50, 50, 50)
+           game.workspace["Mushroom King"].HumanoidRootPart.Transparency = 0.5
+          end
+      end
+      wait()
+  end
+  if MushroomKingHitbox == false then
+   for i, v in pairs(game.workspace:GetChildren()) do 
+      if v.Name == "Mushroom King" and v:FindFirstChild("HumanoidRootPart") then
+         game.workspace["Mushroom King"].HumanoidRootPart.Size = Vector3.new(10.8166, 10.8166, 5.40831)
+         game.workspace["Mushroom King"].HumanoidRootPart.Transparency = 1
+        end
+    end
+   end
+
       end,
    })
-   
-   local DogButton = BossTab:CreateButton({
-      Name = "Dog Hitbox",
-      Callback = function()
-         game.workspace.Dog.HumanoidRootPart.Size = Vector3.new(50, 50, 50)
-      game.workspace.Dog.HumanoidRootPart.Transparency = 0.5
-      
-      end,
-   })
-   
-   local CatButton = BossTab:CreateButton({
+   local CatToggle = BossTab:CreateToggle({
       Name = "Cat Hitbox",
-      Callback = function()
-         game.workspace.Cat.HumanoidRootPart.Size = Vector3.new(50, 50, 50)
-      game.workspace.Cat.HumanoidRootPart.Transparency = 0.5
-      
+      CurrentValue = false,
+      Flag = "CHT", 
+      Callback = function(Value)
+          getgenv().CatHitbox = Value
+          while CatHitbox == true do
+          for i, v in pairs(game.workspace:GetChildren()) do
+          if v.Name == "Cat" and v:FindFirstChild("HumanoidRootPart") then
+           game.workspace.Cat.HumanoidRootPart.Size = Vector3.new(50, 50, 50)
+           game.workspace.Cat.HumanoidRootPart.Transparency = 0.5
+          end
+      end
+      wait()
+  end
+  if CatHitbox == false then
+   for i, v in pairs(game.workspace:GetChildren()) do 
+      if v.Name == "Cat" and v:FindFirstChild("HumanoidRootPart") then
+         game.workspace.Cat.HumanoidRootPart.Size = Vector3.new(10.8166, 10.8166, 5.40831)
+         game.workspace.Cat.HumanoidRootPart.Transparency = 1
+        end
+    end
+   end
+
+
       end,
    })
-   
-   local ScareCrowButton = BossTab:CreateButton({
+   local DogToggle = BossTab:CreateToggle({
+      Name = "Dog Hitbox",
+      CurrentValue = false,
+      Flag = "DHT", 
+      Callback = function(Value)
+
+          getgenv().DogHitbox = Value
+          while DogHitbox == true do
+          for i, v in pairs(game.workspace:GetChildren()) do
+          if v.Name == "Dog" and v:FindFirstChild("HumanoidRootPart") then
+           game.workspace.Dog.HumanoidRootPart.Size = Vector3.new(50, 50, 50)
+           game.workspace.Dog.HumanoidRootPart.Transparency = 0.5
+          end
+      end
+      wait()
+  end
+
+  if DogHitbox == false then
+   for i, v in pairs(game.workspace:GetChildren()) do 
+      if v.Name == "Dog" and v:FindFirstChild("HumanoidRootPart") then
+         game.workspace.Dog.HumanoidRootPart.Size = Vector3.new(10.8166, 10.8166, 5.40831)
+         game.workspace.Dog.HumanoidRootPart.Transparency = 1
+        end
+    end
+   end
+
+
+      end,
+   })
+   local SCToggle = BossTab:CreateToggle({
       Name = "Scarecrow Hitbox",
-      Callback = function()
-         game.workspace.Scarecrow.HumanoidRootPart.Size = Vector3.new(50, 50, 50)
-      game.workspace.Scarecrow.HumanoidRootPart.Transparency = 0.5
-      
+      CurrentValue = false,
+      Flag = "SHT", 
+      Callback = function(Value)
+          getgenv().ScarecrowHitbox = Value
+          while ScarecrowHitbox == true do
+          for i, v in pairs(game.workspace:GetChildren()) do
+          if v.Name == "Scarecrow" and v:FindFirstChild("HumanoidRootPart") then
+           game.workspace.Scarecrow.HumanoidRootPart.Size = Vector3.new(50, 50, 50)
+           game.workspace.Scarecrow.HumanoidRootPart.Transparency = 0.5
+          end
+      end
+      wait()
+  end
+
+  if ScarecrowHitbox == false then
+   for i, v in pairs(game.workspace:GetChildren()) do 
+      if v.Name == "Scarecrow" and v:FindFirstChild("HumanoidRootPart") then
+         game.workspace.Scarecrow.HumanoidRootPart.Size = Vector3.new(10.8166, 10.8166, 5.40831)
+         game.workspace.Scarecrow.HumanoidRootPart.Transparency = 1
+        end
+    end
+   end
+
       end,
    })
- 
+
 
    local AntiTab = Window:CreateTab("Anti", 4483362458) -- Title, Image
 
@@ -267,28 +348,7 @@ elseif game.PlaceId == 10626447503 then
          end
 
 
-        function hitboxexpandTB()
-      local plyr = game.Players.LocalPlayer     
-      local char = plyr.Character
-      local Hitbox = char.Hitbox
-      
-      Hitbox.Size = Vector3.new(20, 20 ,20)
-      Hitbox.Transparency = 0.5
-      Hitbox.Material = "Water"
-   
-      end
-   
-
-   function hitboxshrinkTB()
-      local plyr = game.Players.LocalPlayer     
-      local char = plyr.Character
-      local Hitbox = char.Hitbox
-      
-      Hitbox.Size = Vector3.new(1.5, 1.5 ,2)
-      Hitbox.Transparency = 1
-      Hitbox.Material = "Plastic"
-   
-      end
+ 
 
       
 function invadeblue()
@@ -357,19 +417,41 @@ function invadeblue()
       },
       })
       
-      local HitboxExpandButtonTB = StuffTab:CreateButton({
-         Name = "Expand Hitbox",
-         Callback = function()
-         hitboxexpandTB()
-         
-         end,
-      })
+      local HitboxToggleTB = StuffTab:CreateToggle({
+         Name = "Expand Hitboxes",
+         CurrentValue = false,
+         Flag = "HBTTB", 
+         Callback = function(Value)
+            getgenv().HitboxExpand = Value
+   
+   
+            while HitboxExpand == true do
+               task.wait(0.1)
+           for i, Player in pairs(game.Players:GetPlayers()) do
+               local allplyrschar = Player.Character or Player.CharacterAdded or game.workspace[Player.Name]
+               local HumanoidRootPart = allplyrschar:GetChildren("HumanoidRootPart")
       
-      local NormalButtonTB = StuffTab:CreateButton({
-         Name = "Normal HitBox",
-         Callback = function()
-         hitboxshrinkTB()
-         
+                   if allplyrschar and Player ~= game.Players.LocalPlayer and HumanoidRootPart then
+                      allplyrschar:WaitForChild("HumanoidRootPart").Size = Vector3.new(20, 20, 20)
+                      allplyrschar:WaitForChild("HumanoidRootPart").Transparency = 0.5
+                   end 
+      
+                  end
+                   task.wait()
+               end
+               
+           
+      
+           if HitboxExpand == false then
+              for i, Player in pairs(game.Players:GetPlayers()) do
+                  local allplyrschar = Player.Character or Player.CharacterAdded or game.workspace[Player.Name]
+                      if allplyrschar and Player ~= game.Players.LocalPlayer and game.workspace[Player.Name].HumanoidRootPart then
+                         allplyrschar.HumanoidRootPart.Size = Vector3.new(2, 2, 1)
+                         allplyrschar.HumanoidRootPart.Transparency = 1
+                      end 
+                      
+                  end
+              end
          end,
       })
       
@@ -435,32 +517,7 @@ local IRButtonTB = IslandsTab:CreateButton({
 
 
 elseif game.PlaceId == 10201596675 then
-   function AntiStorm()
-      game.workspace.Storm:Destroy()
-      game.workspace.StormHitbox:Destroy()
-      end
 
-   function hitboxexpandD()
-      local plyr = game.Players.LocalPlayer     
-      local char = plyr.Character
-      local Hitbox = char.Hitbox
-      
-      Hitbox.Size = Vector3.new(20, 20 ,20)
-      Hitbox.Transparency = 0.5
-      Hitbox.Material = "Water"
-   
-      end
-   
-   function hitboxshrinkD()
-      local plyr = game.Players.LocalPlayer     
-      local char = plyr.Character
-      local Hitbox = char.Hitbox
-      
-      Hitbox.Size = Vector3.new(1.5, 1.5 ,2)
-      Hitbox.Transparency = 1
-      Hitbox.Material = "Plastic"
-   
-      end
 
       function destroynametagD()
          local LocalPlayer = game.Players.LocalPlayer
@@ -470,7 +527,7 @@ elseif game.PlaceId == 10201596675 then
          end
 
 
-         local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+   local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
    
    local Window = Rayfield:CreateWindow({
       Name = "dasdres script",
@@ -515,23 +572,45 @@ elseif game.PlaceId == 10201596675 then
    },
    })
    
- 
+   local HitboxToggleD = StuffTab:CreateToggle({
+      Name = "Expand Hitboxes",
+      CurrentValue = false,
+      Flag = "HBTD", 
+      Callback = function(Value)
+         getgenv().HitboxExpand = Value
 
-   local HitboxExpandButtonD = StuffTab:CreateButton({
-      Name = "Expand Hitbox",
-      Callback = function()
-      hitboxexpandD()
-      
-      end,
-   })
+
+         while HitboxExpand == true do
+            task.wait(0.1)
+        for i, Player in pairs(game.Players:GetPlayers()) do
+            local allplyrschar = Player.Character or Player.CharacterAdded or game.workspace[Player.Name]
+            local HumanoidRootPart = allplyrschar:GetChildren("HumanoidRootPart")
    
-   local NormalButtonD = StuffTab:CreateButton({
-      Name = "Normal HitBox",
-      Callback = function()
-      hitboxshrinkD()
-      
+                if allplyrschar and Player ~= game.Players.LocalPlayer and HumanoidRootPart then
+                   allplyrschar:WaitForChild("HumanoidRootPart").Size = Vector3.new(20, 20, 20)
+                   allplyrschar:WaitForChild("HumanoidRootPart").Transparency = 0.5
+                end 
+   
+               end
+                task.wait()
+            end
+            
+        
+   
+        if HitboxExpand == false then
+           for i, Player in pairs(game.Players:GetPlayers()) do
+               local allplyrschar = Player.Character or Player.CharacterAdded or game.workspace[Player.Name]
+                   if allplyrschar and Player ~= game.Players.LocalPlayer and game.workspace[Player.Name].HumanoidRootPart then
+                      allplyrschar.HumanoidRootPart.Size = Vector3.new(2, 2, 1)
+                      allplyrschar.HumanoidRootPart.Transparency = 1
+                   end 
+                   
+               end
+           end
       end,
    })
+
+
    
    local DNTButtonD = StuffTab:CreateButton({
       Name = "Destroy Nametag",
