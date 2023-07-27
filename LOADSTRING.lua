8494a27ec550e65a050ab0dc9673b0d41cfbd6dd
dasdres <137342122+dasdres@users.noreply.github.com>
2023-07-27 19:59:43 +0300

    Add files via upload

---
+++ LOADSTRING.lua
@@ -0,0 +1,7 @@
+if game.PlaceId == 8260276694 or game.PlaceId == 10626447503 or game.PlaceId == 10201596675 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/dasdres/dasdres-hub/main/AWScript.lua"))()
+elseif game.PlaceId == 1962086868 or game.PlaceId == 3582763398 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/dasdres/dasdres-hub/main/TOHscript.lua"))()
+else 
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/dasdres/dasdres-hub/main/Universal.lua"))()
+end
